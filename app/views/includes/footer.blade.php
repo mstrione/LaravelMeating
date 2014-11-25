adf24bd44bfaaceca55547336b6a610ea509e960
Ezequiel <eze_torress@hotmail.com>
2014-11-24 23:34:29 -0300

    Estructura
    Se reacomoda el proyecto, estructurandolo para simplificar codigo.
    
    Se crean 3 carpetas principales: en este comiteo van 2.
    Includes:
    Contiene a head.blade.php el cual posee los meta, y carga el bootstrap.
    header.blade.php contiene la barra de navegacion a las paginas
    footer.blade.php contiene el codigo del pie de pagina que se veran en
    todas las paginas.
    
    En la carpeta layout:
    default.blade.php contiene la estructura por default para la
    visualizacion de las paginas.

---
+++ app/views/includes/footer.blade.php
@@ -0,0 +1,14 @@
+<div id="footer">
+		<!-- FOOTER -->
+        <footer id="mainFooter">
+            <div class="wrapped" align="center"> <!--Anclamos un footer abajo del todo de la pagina-->
+                <p class="pull-right"><a id="goTop" href="#"><h3> ^ </h3></a></p> <!--con ese icono nos lleva hacia arriba de la pagina-->
+                <p>© 2014 MEating   ·  <a href="ruta de privacidad y terminos">Privacidad y Términos</a> · Seguinos en <!--nos va a llevar a los links mencionados abajo a traves de los iconos-imagenes-->
+                	<a href="http://facebook.com"><img src="img/f1.png" height='30' width='70'></a> | 
+					<a href="http://twitter.com"><img src="img/t1.png" height='30' width='70'></a> |
+					<a href="http://plus.google.com/share"><img src="img/g1.png" height='30' width='70'></a>
+					
+                </p>
+            </div>
+        </footer>
+ </div>
---
+++ app/views/includes/header.blade.php
@@ -0,0 +1,19 @@
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container"><img class="navbar-brand" src="img/meatingLogo3.png"   HEIGHT="100px" ></img> <!--style="width: 150px; height: auto;-->
+          <div class="navbar-header">
+              <a class="navbar-brand">Meating</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="/">Inicio</a></li>
+              <li><a href="/about">¿Que es Meating?</a></li>
+              <li><a href="/aboutUs">¿Quienes somos?</a></li> 
+              <li><a href="/contacto">Contacto</a></li>           
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/login">Inicio de Sesion</a></li> 
+              <li><a href="/registro">Registrarse</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+    </div>
