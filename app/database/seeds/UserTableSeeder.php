780e3441cad2610c46b615e590a3d9f109b75d9e
Ezequiel <eze_torress@hotmail.com>
2014-12-02 23:09:35 -0300

    seeds
    se suben los archivos para generar un usuario a traves de la consola
    php artisan db:seed
    Asi crean los usuarion con la contraseña encriptada de manera manual

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Agregamos un usuario nuevo a la base de datos.
+*/
+class UserTableSeeder extends Seeder {
+    public function run(){
+        Usuario::create(array(
+            'username'  => 'admin',
+            'email'     => 'admin@admin.com',
+            //'name'=> 'Administrator',
+            'password' => Hash::make('admin') // Hash::make() nos va generar una cadena con nuestra contraseña encriptada
+        ));
+    }
+}
