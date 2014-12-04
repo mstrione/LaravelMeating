e2250a2b214d8c818bba037cb51d2f2227f04ad8
Ezequiel <eze_torress@hotmail.com>
2014-12-04 02:47:04 -0300

    modelo de item
    eliminacion de codigo innecesario

--- app/models/Item.php
+++ app/models/Item.php
@@ -18,40 +18,7 @@ class Item extends Eloquent {
 	}
 //------------------------------------------------	
 	
-	public static function agregarItem($input)
-	//esta funcion va a RECIBIR COMO PARAMETRO TODA LA INFO DEL FORMULARIO para crear el Item
-	{
-		$respuesta = array();
-		
-		//declaro las reglas para validar el nombre del item y la cantidad
-		$reglas=array(
-			'descripcion'=>array('required', 'max:30',  'alpha_num'),
-			'cantidad'=>array('required','numeric'),
-			);
-			
-		$validator= Validator::make($input, $reglas);
-		
-		//ahora verifico que los datos cumplan con la validacion
-		if ($validator->fails())
-		{
-			//si no cumple las reglas, se devuelven los errores al controlador
-			$respuesta['mensaje']=$validator;
-			$respuesta['error']=true;
-			
-		}else{
-			// en caso de que se cumplan las reglas se crea el objeto Item
-			
-			$item=static::create($input);
-			
-			//ahora retorno un mensaje de exito al controlador
-			$respuesta['mensaje']='Item creado!';
-			$respuesta['error']=false;
-			$respuesta['data']= $item;
-			}
-		return $respuesta;
-			
-		
-	}
+	
 	
 }
 ?>
