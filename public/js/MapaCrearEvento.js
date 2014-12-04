fbc93d35177deb1ef5b1e33d5a4f8ee57aaaa65e
Javier1990 <javiermorabes@gmail.com>
2014-12-04 13:53:03 -0200

    modificacion de js de mapa
    se modifico el java script del mapa para que solo aparezca un marcador
    en vez de n marcadores, todavía queda la duda de como recuperar las
    coordenadas de este marcador

--- public/js/MapaCrearEvento.js
+++ public/js/MapaCrearEvento.js
@@ -3,33 +3,39 @@
 // The user can then click an option to hide, show or delete the markers.
 var map;
 var markers = [];
+var marcador=0;
 
 function initialize() {
-  var haightAshbury = new google.maps.LatLng(-41.1309137, -71.3028207);
+  var haightAshbury = new google.maps.LatLng(-41.1416606, -71.3053098);
   var mapOptions = {
-    zoom: 12,
+    zoom: 14,
     center: haightAshbury,
-    mapTypeId: google.maps.MapTypeId.TERRAIN
+    //mapTypeId: google.maps.MapTypeId.TERRAIN
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
   // This event listener will call addMarker() when the map is clicked.
-  google.maps.event.addListener(map, 'click', function(event) {
-    addMarker(event.latLng);
-  });
+  google.maps.event.addListener(map, 'click', function(event) { addMarker(event.latLng); });
 
   // Adds a marker at the center of the map.
-  addMarker(haightAshbury);
+  //addMarker(haightAshbury);
 }
 
 // Add a marker to the map and push to the array.
-function addMarker(location) {
-  var marker = new google.maps.Marker({
+function addMarker(location) 
+{
+  if (marcador==0) 
+  {
+  var marker = new google.maps.Marker
+  ({
     position: location,
     map: map
   });
+  marcador=marcador+1;
+
   markers.push(marker);
+};//del if
 }
 
 // Sets the map on all markers in the array.
@@ -42,6 +48,7 @@ function setAllMap(map) {
 // Removes the markers from the map, but keeps them in the array.
 function clearMarkers() {
   setAllMap(null);
+  marcador=0
 }
 
 // Shows any markers currently in the array.
@@ -54,5 +61,3 @@ function deleteMarkers() {
   clearMarkers();
   markers = [];
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
