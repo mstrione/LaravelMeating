e2972b78f8ac0b38bbd0a830b19579385e599a6b
Ezequiel <eze_torress@hotmail.com>
2015-03-23 22:01:55 -0300

    crear evento
    opciones para fecha hora y mapa pendientes

--- app/views/pages/crearEvento.blade.php
+++ app/views/pages/crearEvento.blade.php
@@ -7,11 +7,87 @@
 	{{ HTML::script('js/MapaCrearEvento.js') }}
 	{{ HTML::style('css/datepicker.css') }}
 	{{ HTML::style('css/EstiloMapa.css') }}
-	
+	 <style>
+      html, body, #map-canvas {
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      }
+      #panel {
+        position: absolute;
+        top: 5px;
+        left: 50%;
+        margin-left: -180px;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+      }
+    </style>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+    <script>
+// In the following example, markers appear when the user clicks on the map.
+// The markers are stored in an array.
+// The user can then click an option to hide, show or delete the markers.
+var map;
+var markers = [];
 
- <script >google.maps.event.addDomListener(window, 'load', initialize);
-</script>
+function initialize() {
+  var haightAshbury = new google.maps.LatLng(-41.1309137, -71.3028207);
+  var mapOptions = {
+    zoom: 12,
+    center: haightAshbury,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // This event listener will call addMarker() when the map is clicked.
+  google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng);
+  });
+
+  // Adds a marker at the center of the map.
+  addMarker(haightAshbury);
+}
+
+// Add a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setAllMap(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+	<script >google.maps.event.addDomListener(window, 'load', initialize);
+	</script>
 </head>
 @section('content')
 <div class="page-header">
@@ -21,6 +97,7 @@
 </div>
 <div class="jumbotron">
 {{Form::open(array('method'=>'POST','Action'=> '/MisEventos' ,'role'=>'form','class'=>'form-horizontal'))}}
+{{Form::open(array('method'=>'POST','url'=>'pages/Evento','role'=>'form','class'=>'form-horizontal'))}}
 <fieldset>
 			
 			
@@ -42,6 +119,7 @@
 					{{Form::label('Fecha','',array('class'=>'col-lg-1 control-label'))}}
 					<div class="col-lg-10">
 					{{ Form::input('date','Fecha','',array( 'date_format' => 'yyyy-mm-dd')) }}
+					{{ Form::input('date', 'fecha') }}
 					<!--Form::text('fecha','',array('class'=>'form-control','class'=>'input-append date','data-date-format'=>'dd-mm-yyyy'))-->
 					</div>
 					<span class="add-on"><i class="icon-th"></i></span>
@@ -54,6 +132,7 @@
 						{{Form::label('Hora','',array('class'=>'col-lg-1 control-label'))}}
 						<div class="col-lg-10">
 						{{ Form::input('time', 'hora','',array( 'time_format' => 'HH:mm:ss')) }}
+						{{ Form::input('time', 'hora') }}
 						<!--Form::text('hora','',array('class'=>'form-control')) -->
 						</div>
 				</div>
